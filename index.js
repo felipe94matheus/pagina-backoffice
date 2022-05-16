88d7dd730444b806354cb470ed4af633e4366992
Felipe server <felipe774@gmail.com>
2022-05-16 13:53:13 -0300

    criado os alert's do deletar e cadastrar

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-})
+// myModal.addEventListener('shown.bs.modal', () => {
+//     myInput.focus()
+// })
 
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 
@@ -22,6 +22,40 @@ const alert = (message, type) => {
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
     alertTrigger.addEventListener('click', () => {
-        alert('Nice, you triggered this alert message!', 'success')
+        alert('Produto cadastrado com sucesso!', 'success')
+    })
+}
+
+// ------------------------------------------------------------------------
+const alertDeletePlaceholder = document.getElementById('deleteAlertPlaceholder')
+
+const alertDelete = (message, type) => {
+    const wrapperDelete = document.createElement('div')
+    wrapperDelete.innerHTML = [
+        `<div class="alert alert-danger alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertDeletePlaceholder.append(wrapperDelete)
+}
+
+const alertTriggerDelete1 = document.querySelector('.delete1')
+if (alertTriggerDelete1) {
+    alertTriggerDelete1.addEventListener('click', () => {
+        alertDelete('Produto deletado com sucesso!', 'success')
+    })
+}
+const alertTriggerDelete2 = document.querySelector('.delete2')
+if (alertTriggerDelete2) {
+    alertTriggerDelete2.addEventListener('click', () => {
+        alertDelete('Produto deletado com sucesso!', 'success')
+    })
+}
+const alertTriggerDelete3 = document.querySelector('.delete3')
+if (alertTriggerDelete3) {
+    alertTriggerDelete3.addEventListener('click', () => {
+        alertDelete('Produto deletado com sucesso!', 'success')
     })
 }
